5565cc4376eb3b1f0b14325bee41116803de26a2
Dwaynewisdom <Dwaynewisdom5@gmail.com>
2026-04-14 07:41:42 -0500

    About page

--- src/Pages/about.jsx
+++ src/Pages/about.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
+import {gsap} from 'gsap';
+import { useEffect } from 'react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 
 function About() {
+  useEffect(() => {
+    gsap.to("#about,#about3", {
+      scrollTrigger: {
+        trigger: "#about-container",
+        start: "top 80%",
+        scrub: true,
+      },
+      rotate: 0,
+      opacity: 1,
+    });
+  }, [])
   return (
-    <div className='bg-[url("/Noise.png")] min-h-screen w-full relative overflow-hidden'>
+    <div id='about-container' className=' min-h-screen w-full relative overflow-hidden'>
         <div className='flex flex-col items-center justify-center h-screen px-20 w-full'>
-            <h1> What are we?</h1>
-            <p>We are a team of passionate professionals dedicated to delivering exceptional results for our clients. With years of experience in our field, we bring a unique blend of creativity and technical expertise to every project we undertake.</p>
+            <div id="about" className='bg-[#000000b3] absolute -rotate-20 mr-50 mt-10 rounded-lg p-10 w-40 h-60 max-w-2xl text-center'>
+            
+            </div>
+            <div id="about2" className='bg-[#000000b3] absolute rounded-lg p-10 w-50 h-70 max-w-2xl text-center'>
+            
+            </div>
+            <div id="about3" className='bg-[#000000b3] absolute rotate-20 ml-50 mt-10 rounded-lg p-10 w-40 h-60 max-w-2xl text-center'>
+            
+            </div>
+        </div>
+        <div>
+
         </div>
     </div>
   )

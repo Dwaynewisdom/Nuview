25e27b3d39901caf85cc36307c198113a3cff3f0
Dwayne Wisdom <Dwaynewidsom5@gmail.com>
2026-04-14 19:31:49 -0400

    About page

--- src/Pages/about.jsx
+++ src/Pages/about.jsx
@@ -7,32 +7,28 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 function About() {
-  useEffect(() => {
-    gsap.to("#about,#about3", {
-      scrollTrigger: {
-        trigger: "#about-container",
-        start: "top 80%",
-        scrub: true,
-      },
-      rotate: 0,
-      opacity: 1,
-    });
-  }, [])
+
+  const offer =({
+    Number:"1",
+    Offer:"Web Design & Ui",
+    Explanation:"",
+    tools:""
+  })
+
   return (
     <div id='about-container' className=' min-h-screen w-full relative overflow-hidden'>
-        <div className='flex flex-col items-center justify-center h-screen px-20 w-full'>
-            <div id="about" className='bg-[#000000b3] absolute -rotate-20 mr-50 mt-10 rounded-lg p-10 w-40 h-60 max-w-2xl text-center'>
-            
-            </div>
-            <div id="about2" className='bg-[#000000b3] absolute rounded-lg p-10 w-50 h-70 max-w-2xl text-center'>
-            
-            </div>
-            <div id="about3" className='bg-[#000000b3] absolute rotate-20 ml-50 mt-10 rounded-lg p-10 w-40 h-60 max-w-2xl text-center'>
-            
-            </div>
-        </div>
         <div>
+          
+          <h1 className='text-5xl font-bold  mt-25 ml-5  '>
+            Built to <span className='text-orange-400'>Perform</span>
+          </h1>
 
+          <p className='mt-5 text-2xl w-70 ml-5 font-[space-mono]'>
+            We don't just build Websites. We engineer digital platforms that convert visitors into customers, scale with your ambitions, and outlast trends.
+          </p>
+          <div>
+
+          </div>
         </div>
     </div>
   )
